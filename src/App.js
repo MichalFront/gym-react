5b39d67b6f7a6a720ce53418beb9f08480383161
Micha Zawadzki <michalzawadzki@MacBook-Air-Micha.local>
2023-12-07 01:44:24 +0100

    New files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
