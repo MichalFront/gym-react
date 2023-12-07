6d4b9d19fc84891aeb026a39a9a43a926d3067e4
Micha Zawadzki <michalzawadzki@MacBook-Air-Micha.local>
2023-12-07 22:49:10 +0100

    New files0712

---
+++ src/Components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import aboutimage from '../images/about1.jpg';
+
+function About(){
+    return(
+        <div id="about">
+            <div className="about-image">
+                <img src={aboutimage} alt=""></img>
+            </div>
+            <div className="about-text">
+                <h1>Learn more about us</h1>
+                <p>Lorem ipsum dolor sit, amet conse</p>
+                <button>Read more</button>
+            </div>
+        </div>
+    );
+}
+
+export default About;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import logo from '../images/logo.png';
+import logo from '../images/logo2.png';
 import {Link} from 'react-scroll';
 
 function Navbar(){
@@ -15,7 +15,7 @@ function Navbar(){
     window.addEventListener('scroll',changeBackground);
     return(
         <nav className={nav ? "nav active": "nav"}>
-            <Link to='#' className='logo'>
+            <Link to='main' className='logo' smooth={true} duration={2000}>
                 <img src={logo} alt='2'/>
             </Link>
             <input className='menu-btn' type='checkbox' id='menu-btn'/>
@@ -23,11 +23,11 @@ function Navbar(){
                 <span className='nav-icon'></span>
             </label>
             <ul className='menu'>
-                <li><Link to='#'>Header</Link></li>
-                <li><Link to='#'>Features</Link></li>
-                <li><Link to='#'>Offer</Link></li>
-                <li><Link to='#'>About</Link></li>
-                <li><Link to='#'>Contact</Link></li>
+                <li><Link to='main' smooth={true} duration={1000}>Header</Link></li>
+                <li><Link to='features' smooth={true} duration={1000}>Features</Link></li>
+                <li><Link to='presentaion' smooth={true} duration={1000}>Offer</Link></li>
+                <li><Link to='about' smooth={true} duration={1000}>About</Link></li>
+                <li><Link to='contact' smooth={true} duration={1000}>Contact</Link></li>
             </ul>
         </nav>
     );
